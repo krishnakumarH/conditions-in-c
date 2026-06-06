e48d2151f6f25590f37839a3dccbfc6bab243b34
Krishna kumar <krishnakumar50621@gmail.com>
2026-06-06 11:44:10 +0530

    Update storage_class_diff.c

--- storage_class_diff.c
+++ storage_class_diff.c
@@ -7,7 +7,7 @@ func( )
 {
 auto int i = 0 ;      //fun1=1, func2=1
 register int j = 0 ;   //fun1=1, fun2=1
-static int k = 0 ;      //fun1=1,fun2=2 bcz it remember data
+static int k = 0 ;      //fun1=1,fun2=2 bcz it remember data value retained)
 i++ ; j++ ; k++ ;
 printf ( "\n %d % d %d", i, j, k ) ; op will be fun1=111,fun2=112
 }
