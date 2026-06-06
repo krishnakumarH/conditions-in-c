bc93dd37cec452a397031f82acb77c3d4ec9804d
Krishna kumar <krishnakumar50621@gmail.com>
2026-06-06 12:47:35 +0530

    Update storage_class_diff.c

--- storage_class_diff.c
+++ storage_class_diff.c
@@ -11,3 +11,7 @@ static int k = 0 ;      //fun1=1,fun2=2 bcz it remember data value retained)
 i++ ; j++ ; k++ ;
 printf ( "\n %d % d %d", i, j, k ) ; op will be fun1=111,fun2=112
 }
+
+extern->used to access global variables,allows diff file to access
+(refer to existing variable),<-this is mem
+reg->used to run in cpu ,no mem 
